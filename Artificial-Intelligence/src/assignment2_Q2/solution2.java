48dbeb8334cf1d538a7a3ebb515d38fe5ad4592f
zirui <ziruiproject@windowsdesktop.com>
2020-12-19 23:18:42 +0000

    Artificial Intelligence Assignment 2

---
+++ Artificial-Intelligence/src/assignment2_Q2/solution2.java
@@ -0,0 +1,253 @@
+package assignment2_Q2;
+import java.awt.BorderLayout;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+
+import org.knowm.xchart.*;
+import org.knowm.xchart.style.markers.SeriesMarkers;
+
+import javax.swing.*;
+import java.awt.*;
+import java.util.Collections;
+import java.util.List;
+
+public class solution2 {
+
+    public static final String DATA_FILE = "data/Q2data.csv";
+
+    public static void main(String[] args) {
+
+        // -------------------------------------------------
+        // Data and Graph setup.
+        // -------------------------------------------------
+        List<List<Double>> data = Data.dataFrom(DATA_FILE);
+        Plot plt = new Plot("Height vs Finger Length", "Height", "Finger Length", data);
+        sleep(500);
+
+        // -------------------------------------------------
+        // Gradient Descent
+        // -------------------------------------------------
+        final int epochs = 100;  // Number of iterations we want to run through the algorithm
+
+        // We want to predict h(x) = w1 * x + w0
+        double w2 = 0;
+        double w1 = 0;
+        double w0 = 0;
+
+        // Learning rate
+        double alpha = 0.1;
+
+        // Main Gradient Descent Function for Linear Regression
+        for(int i = 0; i < epochs; i++) {
+
+            double cost = 0;
+
+            for(int j = 0; j < data.get(0).size(); j++) {
+
+                double x1_j = data.get(0).get(j);
+                double x2_j = data.get(1).get(j);
+                double y_j = data.get(2).get(j); 
+
+                double prediction = 1/(1+Math.exp(-(w0 + (w1 * x1_j)+w2 *x2_j )));
+
+                // cost += (y_j - h(x))^2
+//                cost += (y_j - prediction) * (y_j - prediction);
+                if(y_j==1) {
+                	cost += Math.log(w0+w1*x1_j+w2*x2_j);
+                }
+                if(y_j==0) {
+                	cost += Math.log(1 - (w0+w1*x1_j+w2*x2_j));
+                }
+
+                // Update the parameters for our equation.
+                w2 += alpha * (y_j - prediction) * x2_j;
+                w1 += alpha * (y_j - prediction) * x1_j;
+                w0 += alpha * (y_j - prediction);
+
+            }
+
+            System.out.println("Current Cost: " + cost);
+
+
+            // ---------------------------------------------
+            // Our Hypothesis Function after the epoch
+            // (these values are final because of how
+            // functional programming works in Java).
+            final double w_2 = w2;
+            final double w_1 = w1;
+            final double w_0 = w0;
+            HypothesisFunction h_x = (x) -> (-w_1*x-w_0)/w_2;
+            // ----------------------------------------------
+            // Plotting prediction with current values of w
+            plt.updatePlot(h_x);
+            sleep(50);
+            // ----------------------------------------------
+        }
+
+
+        System.out.println("Final Equation: h(x) = ("+ w2 +" * x1) + (" + w1 + " * x2) + (" + w0 + ")");
+    }
+
+    static void sleep(int ticks) {
+        try{ Thread.sleep(ticks); } catch(Exception e) { e.printStackTrace(); }
+    }
+}
+
+
+
+
+
+
+class Data {
+
+    /*
+     * Reads the data from a given csv file
+     */
+    public static List<List<Double>> dataFrom(String fileName) {
+        List<List<Double>> output = new ArrayList<>();
+
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(fileName));
+            String line;
+
+            while((line = reader.readLine()) != null) {
+                String[] rawValues = line.split(",");
+                for(int i = 0; i < rawValues.length; i++) {
+                    if(i >= output.size()) output.add(new ArrayList<>());
+                    output.get(i).add(Double.parseDouble(rawValues[i]));
+                }
+            }
+
+            reader.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return output;
+    }
+
+    /*
+     * Generates the points to plot a function.
+     */
+    public static List<List<Double>> plotFunction(double min, double max, double step, HypothesisFunction f_x){
+        List<List<Double>> output = new ArrayList<>();
+        output.add(new ArrayList<>());
+        output.add(new ArrayList<>());
+
+        for(double x = min; x <= max; x += step) {
+            output.get(0).add(x);
+            output.get(1).add(f_x.evaluate(x));
+        }
+
+        return output;
+    }
+}
+
+interface HypothesisFunction {
+    double evaluate(double x);
+}
+
+
+
+class Plot {
+
+    XYChart chart;
+    double yMin;
+    double yMax;
+    double xMin;
+    double xMax;
+    JFrame frame;
+
+    public Plot(String title, String xAxis, String yAxis, List<List<Double>> data) {
+
+        // Create the chart
+        chart = new XYChartBuilder()
+                        .width(1000)
+                        .height(600)
+                        .title(title)
+                        .xAxisTitle(xAxis)
+                        .yAxisTitle(yAxis)
+                        .build();
+
+        // Finding the domain and range to plot the graph in
+        this.yMin = yMin(data) - 3;
+        this.yMax = yMax(data) + 3;
+        this.xMin = xMin(data) - 3;
+        this.xMax = xMax(data) + 3;
+
+        // Set the domain and range of the graph.
+        chart.getStyler().setYAxisMin(yMin);
+        chart.getStyler().setYAxisMax(yMax);
+        chart.getStyler().setXAxisMin(xMin);
+        chart.getStyler().setXAxisMax(xMax);
+
+        // Create the scatter plot of the data
+        XYSeries raw = chart.addSeries("raw", data.get(0), data.get(1));
+        raw.setXYSeriesRenderStyle(XYSeries.XYSeriesRenderStyle.Scatter);
+
+        // Plot the initial function with multiple points and join them with a line
+        List<List<Double>> plotPoints = Data.plotFunction(xMin, xMax, .1, (x)->0);
+
+        // To draw the function.
+        XYSeries plt = chart.addSeries("plot", plotPoints.get(0), plotPoints.get(1));
+        plt.setXYSeriesRenderStyle(XYSeries.XYSeriesRenderStyle.Line);
+        plt.setShowInLegend(false);
+        plt.setMarker(SeriesMarkers.NONE);
+
+        SwingUtilities.invokeLater(() -> {
+
+                // Create and set up the window.
+                frame = new JFrame(title);
+                frame.setLayout(new BorderLayout());
+                frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+                // chart
+                JPanel chartPanel = new XChartPanel<>(chart);
+                frame.add(chartPanel, BorderLayout.CENTER);
+
+                // Display the window.
+                frame.pack();
+                frame.setVisible(true);
+        });
+
+    }
+
+    private double yMin(List<List<Double>> data) {
+        double min =  Collections.min(data.get(1));
+        return (min >= 0) ? 0 : min;
+    }
+
+    private double yMax(List<List<Double>> data) {
+        return Collections.max(data.get(1));
+    }
+
+    private double xMin(List<List<Double>> data) {
+        double min =  Collections.min(data.get(0));
+        return (min >= 0) ? 0 : min;
+    }
+
+    private double xMax(List<List<Double>> data) {
+        return Collections.max(data.get(0));
+    }
+
+    public void updatePlot(HypothesisFunction h_x) {
+        List<List<Double>> plotPoints = Data.plotFunction(xMin,xMax, .1, h_x);
+        chart.updateXYSeries("plot",plotPoints.get(0), plotPoints.get(1), null);
+        frame.repaint();
+    }
+
+}
+
+
--- README.md
+++ README.md
@@ -1,1 +1,89 @@
-# Artificial-Intelligence-Assignments
+# Artificial Intelligence Assignments 2
+## Assignment Description
+This assignment will be graded out of 50 and will contribute towards 10% of this module's continuous assessment. 
+
+This assignment consists of 3 (three) questions and you are required to submit two java programs and one pdf file. This assignment only allows for the submission of files with extensions .java and .pdf. 
+
+If you choose to write your solutions out then you must ensure that you write legibly and that any pictures you take of your written work are clear and of a reasonable resolution. Ensure that you maintain the aspect ratio of any images you use. 
+
+Each question has a list of criteria which will be used for grading and this is listed at the end of that question. Carefully read through this before you start working on this assignment. 
+
+Be sure to double check the deadline for this assignment - extensions will be possible ONLY through welfare. Please remember that a delay in submission will lead to a significant delay in grading as we enter the Easter holidays. 
+
+Your assignment begins below.
+
+### Question 1     [ 15 Marks ]
+Recall the Java program discussed in class for Univariate Linear Regression (https://github.com/alfi-s/univariate-linear-regression (Links to an external site.)) 
+
+Write a program to perform Univariate non-linear Regression and test it on the following data where x is the independent variable and y the dependant variable.
+
+![avatar](images/1.png)
+
+Assume that the this hypothesis function is quadratic - this will imply that the hypothesis function is of the form:
+
+hw( x ) = w0 + w1x + w2x2
+
+Run your program through as many epochs as is required before you achieve a reasonable fit. Remember that extremely high values of the learning rate will result in your program not converging. Here are some suggested initial values: w0  = w1 = w2 = 0 and alpha = 0.0001
+
+<u>**You are required to submit the following:**</u>
+
+Your java code (as a file named solution1.java) 
+
+Your pdf containing your write up must include the final values of w0, w1 and w2 along with a plot of the above points and a plot of the curve you have "fit" to this data. An easy way of plotting this is to use a spreadsheet, enter in the training data and to add a column that makes use of a formula to generate values of y. This formula will be your hypothesis function parameterized by w. You are, of course, free to include code to generate this chart within your java program. 
+
+I will discuss details on how to create this chart in class. You are encouraged to start writing the java code before then. 
+
+&nbsp;
+<u>**Grading Criteria:**</u>
+
+Your java code will be evaluated primarily on correctness and clarity, while some weight will be placed on a reasonable number of comments. 
+
+Your write-up will be graded on how accurately your parameterized hypothesis function fits the training data. Any reasonable fit will be accepted. 
+
+Below is an example of what is expected: 
+
+![avatar](images/2.png)
+
+### Question 2     [ 25 Marks ]
+
+Write a program to perform Multivariate Logistic Regression and test it on the following data where x1 and x2 are independent variables and y represents whether or not that input belongs to the class (y=1) or not (y=0)
+
+![avatar](images/3.png)
+
+Recall that the hypothesis function of logistic regression with two independent variables is of the form:
+
+hw( x ) = g(w0 + w1x1 + w2x2)
+
+As in Question 1, run your program through as many epochs as is required before you achieve a reasonable fit. Here are some suggested initial values: w0  = w1 = w2 = 0 and alpha = 0.1
+
+<u>**You are required to submit the following:**</u>
+
+Your java code (as a file named solution2.java) 
+
+Your pdf containing your write up must include the final values of w0, w1 and w2 along with a plot of the above points and a plot of the decision boundary. Note that this chart will be x1 vs x2 and the decision boundary can be calculated using x2 = ( - w1*x1 - w0 ) / w2. Once again, feel free to use a spreadsheet to plot this, alternatively include the plots in your program code. 
+
+<u>**Grading Criteria:**</u>
+
+Your java code will be evaluated primarily on correctness and clarity, while some weight will be placed on a reasonable number of comments. 
+
+Your write-up will be graded on how accurately your parameterized hypothesis function fits the training data. Any reasonable fit will be accepted. 
+
+Below is an example of what is expected: 
+
+![avatar](images/4.png)
+
+### Question 3  [10 Marks ]
+
+Recall the diagram representing a single logistic unit capable of performing the AND operation on two variables discussed during the lecturers. 
+
+Draw a single logistic unit capable of performing the AND operation on THREE variables. Include a table showing the values of x1, x2, x3, z and g(z). Notice that this table will have eight rows. [5 marks]
+
+Draw a single logistic unit capable of performing the OR operation on THREE variables. Include a table showing the values of x1, x2, x3, z and g(z). Notice that this table will have eight rows. [5 marks]
+
+<u>**Grading Criteria:**</u>
+
+Your solution will be graded on correctness of the weights and the correct illustration of the logit.
+
+You will additionally be evaluated on the table you provide detailing the values of z and g(z) for all possible values of x1, x2 and x3. 
+
+End of assignment.
